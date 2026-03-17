e414daab136e6a944a56488bef803591fff4209f
Aditya kumar <adityakumar.net4566@gmail.com>
2026-03-17 16:35:49 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on ${port}`));
+app.listen(port, () => console.log(`Server running on ${port}`));
 
 // Middleware
 app.use(cors({
